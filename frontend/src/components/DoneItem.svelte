99078b9c5b894c78748a8d8c2b69a257ff721273
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-06-20 18:31:26 +0800

    edit DoneItem

--- frontend/src/components/DoneItem.svelte
+++ frontend/src/components/DoneItem.svelte
@@ -2,30 +2,40 @@
   import Fa from "svelte-fa";
   import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-  import { dones } from "./store";
   import { send, receive } from "./animation";
+  import fetchData from "^/fetctData";
+  import { dones } from "~/store";
 
-  export let doneitem: {
-    id: number;
-    desc: string;
+  export let done: {
+    ID: number;
+    subject: string;
   };
 
+  async function refreshData() {
+    const { doneData } = await fetchData();
+    $dones = doneData.dones;
+  }
+
   function removeDone(id: number) {
-    $dones = $dones.filter((t) => t.id !== id);
+    fetch(`http://localhost:8080/done/${id}`, {
+      method: "DELETE",
+    }).then(async () => {
+      await refreshData();
+    });
   }
 </script>
 
 <label
-  in:receive={{ key: doneitem.id }}
-  out:send={{ key: doneitem.id }}
+  in:receive={{ key: done.ID }}
+  out:send={{ key: done.ID }}
   class="relative mx-auto mb-4 flex w-[56%] justify-between border-2 border-red-500 bg-red-300 px-4 py-2 text-xl hover:bg-red-400"
 >
   <div class="flex items-center justify-center">
-    <div>{doneitem.desc}</div>
+    <div>{done.subject}</div>
   </div>
   <button
     on:click={() => {
-      removeDone(doneitem.id);
+      removeDone(done.ID);
     }}
   >
     <div class="flex items-center">
