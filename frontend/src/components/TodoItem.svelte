a2be89f747ae480137a365e11e02431ee2b92857
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-06-20 18:31:08 +0800

    edit TodoItem

--- frontend/src/components/TodoItem.svelte
+++ frontend/src/components/TodoItem.svelte
@@ -2,41 +2,59 @@
   import { faTrash } from "@fortawesome/free-solid-svg-icons";
   import Fa from "svelte-fa";
 
-  import { todos, dones, doneId } from "./store";
   import { send, receive } from "./animation";
+  import fetchData from "^/fetctData";
+  import { todos, dones } from "~/store";
 
   export let todo: {
-    id: number;
-    desc: string;
+    ID: number;
+    subject: string;
   };
 
-  export function removetodo(id: number) {
-    $todos = $todos.filter((t) => t.id !== id);
+  async function refreshData() {
+    const { todoData, doneData } = await fetchData();
+    $todos = todoData.todos;
+    $dones = doneData.dones;
+  }
+
+  function removetodo(id: number) {
+    fetch(`http://localhost:8080/todo/${id}`, {
+      method: "DELETE",
+    }).then(async () => {
+      await refreshData();
+    });
   }
 
   export function addDoneItem(id: number, todo: string) {
     removetodo(id);
-    $dones = [...$dones, { id: $doneId, desc: todo }];
-    $doneId += 1;
+    fetch("http://localhost:8080/done", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ subject: todo }),
+    }).then(async () => {
+      await refreshData();
+    });
   }
 </script>
 
 <label
-  in:receive={{ key: todo.id }}
-  out:send={{ key: todo.id }}
+  in:receive={{ key: todo.ID }}
+  out:send={{ key: todo.ID }}
   class=" relative mx-auto mb-4 flex w-[56%] justify-between border-2 border-green-500 bg-green-300 px-4 py-2 text-xl hover:bg-green-400"
 >
   <div class="flex items-center justify-center">
     <input
-      on:click={() => addDoneItem(todo.id, todo.desc)}
+      on:click={() => addDoneItem(todo.ID, todo.subject)}
       type="checkbox"
       class="mr-4"
     />
-    <div>{todo.desc}</div>
+    <div>{todo.subject}</div>
   </div>
   <button
     on:click={() => {
-      removetodo(todo.id);
+      removetodo(todo.ID);
     }}
   >
     <div class="flex items-center">
